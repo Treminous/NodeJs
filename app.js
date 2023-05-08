ff0e9c050edcd8b17c545b69f630b84c4d15be1a
Geofrey Ongidi <ongidigeofrey@gmail.com>
2023-05-08 11:14:18 +0300

    created server

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-//access global variables
-const names=require('./firstFile');
-const sayHi=require('./sayFile');
-const items=require('./array.js');
-console.log(items);
-require('./addValue');
-//display path
-console.log(__dirname);
-//set time interval
-// setInterval(() => {
-//     console.log('Hello World');
 
-// },1000);
-
-sayHi(names.john);
-sayHi(names.peter);
-sayHi('susan');
+const  server =http.createServer((req,res)=>
+{ 
+    if(req.url==='/')
+    {
+ 
+    res.end('Welcome to NodeJs Programming');
+    }
+    if(req.url==='/about')
+    {
+        res.end('Here is our short story');
+    }
+    res.end(`<h1> Oops!</h1> <p>We can't find such a page <a href='/' </a> Back home</p>`)
+})
+server.listen(5000);
--- http.js
+++ http.js
@@ -1,4 +1,20 @@
+ //access global variables
+const names=require('./firstFile');
+const sayHi=require('./sayFile');
+const items=require('./array.js');
+console.log(items);
+require('./addValue');
+ //display path
+ console.log(__dirname);
+//set time interval
+setInterval(() => {
+     console.log('Hello World');
+
+},1000);
+
+sayHi(names.john);
+sayHi(names.peter);
+sayHi('susan');
 const http=require('http');
-const express=require('express');
-//
-//const server
+
+//Create server
